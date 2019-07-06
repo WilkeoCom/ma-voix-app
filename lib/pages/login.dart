2ad84c59e8b7e9c9d6733a12e0687f59e43fdf42
Alexis Phuong <alexis.hvp@gmail.com>
2019-07-06 10:43:38 +0200

    feat(login): add login business

--- lib/pages/login.dart
+++ lib/pages/login.dart
@@ -1,46 +1,78 @@
 import 'package:flutter/material.dart';
 
-class Login extends StatelessWidget {
+import '../login/mixins/validation_mixin.dart';
+import '../login/services/login_api_provider.dart';
+
+class Login extends StatefulWidget {
+  createState() {
+    return LoginState();
+  }
+}
+
+class LoginState extends State<Login> with ValidationMixin {
+  final formKey = GlobalKey<FormState>();
+
+  String email = '';
+  String password = '';
+  final api = LoginApiProvider();
+
   Widget build(context) {
     return Scaffold(
-        body: Container(
-            margin: EdgeInsets.all(20.0),
-            child: Column(
-              children: [
+      body: Container(
+        margin: EdgeInsets.all(20.0),
+        child: Form(
+          key: formKey,
+          child: Column(
+            children: [
               emailField(),
               passwordField(),
               Container(margin: EdgeInsets.only(top: 25.0)),
               submitButton(),
-            ])));
+            ],
+          ),
+        ),
+      ),
+    );
   }
-}
 
-Widget emailField() {
-  return TextField(
-    keyboardType: TextInputType.emailAddress,
-    decoration: InputDecoration(
-      hintText: 'you@example.com',
-      labelText: 'Email Address',
-    ),
-  );
-}
+  Widget emailField() {
+    return TextFormField(
+      keyboardType: TextInputType.emailAddress,
+      decoration: InputDecoration(
+        hintText: 'you@example.com',
+        labelText: 'Email Address',
+      ),
+      validator: validateEmail,
+      onSaved: (String value) {
+        email = value;
+      },
+    );
+  }
 
-Widget passwordField() {
-  return TextField(
-    obscureText: true,
-    decoration: InputDecoration(
-      hintText: 'Password',
-      labelText: 'Password'
-    )
-  );
-}
+  Widget passwordField() {
+    return TextFormField(
+      obscureText: true,
+      decoration: InputDecoration(
+        hintText: 'Password',
+        labelText: 'Password',
+      ),
+      validator: validatePassword,
+      onSaved: (String value) {
+        password = value;
+      },
+    );
+  }
 
-Widget submitButton() {
-  return RaisedButton(
-    child: Text('Login'),
-    color: Colors.blue,
-    onPressed: (){
-      print('hello');
-    },
-  );
+  Widget submitButton() {
+    return RaisedButton(
+      child: Text('Login'),
+      color: Colors.blue,
+      onPressed: () {
+        if (formKey.currentState.validate()) {
+          formKey.currentState.save();
+          api.token(email, password);
+        }
+      },
+    );
+  }
 }
